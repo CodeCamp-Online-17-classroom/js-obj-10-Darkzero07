25441173a243b1b21b327601260507d9c4419f1e
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-20 07:46:32 +0700

    js lab 56

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-// write code here
+let key = prompt("Enter fruit name:");
+let obj = {};
+while (key !== "stop") {
+    let val = prompt(`Number of ${key} :`);
+    let many = parseInt(val) > 1 ? key+"s" : key;
+    obj[many] = val;
+    key = prompt('fruit name:');
+};
+console.log(obj);
